63f6c5c632aea2edc1b5777a94ea74608f6d3c6f
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-12 22:56:56 +0200

    added User Domain Model

---
+++ src/main/java/edu/spring/security/authorize/domain/model/User.java
@@ -0,0 +1,58 @@
+package edu.spring.security.authorize.domain.model;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+
+@Entity
+@Table
+public class User implements UserDetails, Serializable {
+
+    @Id
+    private Long id;
+
+    private String userName;
+    private String password;
+
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
