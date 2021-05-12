cf0a2254c4c2f54a1e52b64fc2d59db9faec5072
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-12 10:14:50 +0200

    Project Structure set.

---
+++ src/test/java/edu/spring/security/authorize/AuthorizeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.spring.security.authorize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
