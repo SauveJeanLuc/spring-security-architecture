3d6e43dd8874c229e509833677582ac69e1685c7
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-12 21:50:22 +0200

    basic spring configuration

--- src/main/java/edu/spring/security/authorize/controller/HelloRestController.java
+++ src/main/java/edu/spring/security/authorize/controller/HelloRestController.java
@@ -1,7 +1,11 @@
 package edu.spring.security.authorize.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("hello")
 public class HelloRestController {
 
     @GetMapping("user")
