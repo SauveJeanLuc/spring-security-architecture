91cc5b88b64c6dc9e53b07e6389946f768a4b9db
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-24 21:12:43 +0200

    GenerateAccessToken definition

--- src/main/java/edu/spring/security/authorize/config/security/JwtTokenUtil.java
+++ src/main/java/edu/spring/security/authorize/config/security/JwtTokenUtil.java
@@ -4,6 +4,8 @@ import edu.spring.security.authorize.domain.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.*;
+
+import java.util.Date;
 import java.util.logging.Logger;
 
 import static java.lang.String.format;
@@ -20,7 +22,18 @@ public class JwtTokenUtil {
     public String generateAccessToken(User user){
         return Jwts.builder()
                 .setSubject(format("%s,%s", user.getId(), user.getUsername()))
+                .setIssuer(jwtIssuer)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+7*24*60*60*1000)) // To expire in 1 week
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
     }
+
+    public String getUserId(String token){
+
+    }
+
+
 
 
 }
