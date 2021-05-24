b66069800c294c6bad0e0b00268f70bdf1712cdf
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-24 20:55:36 +0200

    initiated JwtTokenFilter class

---
+++ src/main/java/edu/spring/security/authorize/config/security/JwtTokenFilter.java
@@ -0,0 +1,10 @@
+package edu.spring.security.authorize.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenFilter {
+
+}
